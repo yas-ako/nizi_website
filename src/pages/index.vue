0cae973f29b3b59b190ba2ac41f9b3945ed3faaf
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-12-03 23:45:54 +0900

    update: 地図の描画

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,4 +4,9 @@ const route = useRoute();
 
 <template>
   <h1>虹</h1>
+  研究の説明とか？
+
+  <NuxtLink to="map">
+    <v-btn color="info">地図を見る</v-btn>
+  </NuxtLink>
 </template>
---
+++ src/pages/map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <template v-for="item in items">
+      <img
+        class="image"
+        :src="`${url}${29084 + item[0]}/${12886 + item[1]}.png`"
+      />
+    </template>
+  </div>
+</template>
+
+<script setup>
+const url = "https://cyberjapandata.gsi.go.jp/xyz/std/15/";
+const urlTemplate = `https://cyberjapandata.gsi.go.jp/xyz/std/15/29084/12886.png`;
+
+const items = new Array(1600);
+for (let x = 0; x < 40; x++) {
+  for (let y = 0; y < 40; y++) {
+    items[40 * x + y] = [y, x];
+  }
+}
+</script>
+
+<style>
+.container {
+  display: grid;
+  grid-template-columns: repeat(40, 2.5%);
+}
+.image {
+  width: 100%;
+}
+</style>
