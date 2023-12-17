c643284f55fb3fb427b646ff2ef6fb4a7334eff0
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-12-17 18:33:56 +0900

    update: 色の表示方法

--- src/app.vue
+++ src/app.vue
@@ -17,11 +17,9 @@
       </v-container>
     </v-navigation-drawer>
     <v-main>
-      <v-main>
-        <v-container>
-          <NuxtPage />
-        </v-container>
-      </v-main>
+      <v-container>
+        <NuxtPage />
+      </v-container>
     </v-main>
   </v-app>
 </template>
